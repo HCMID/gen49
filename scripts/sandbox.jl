874062b9ddcb7462b856ba08754d5e11f6ca7d6d
Neel Smith <dnsmith.neel@gmail.com>
2022-09-18 11:40:23 -0400

    Parse Gen49 into citable markdown

--- scripts/sandbox.jl
+++ scripts/sandbox.jl
@@ -1,10 +1,10 @@
 using EzXML
-f = joinpath(pwd(), "texts", "editions", "Geneva49.xml")
+using CitableBase, CitableText, CitableCorpus
 
+f = joinpath(pwd(), "texts", "editions", "Geneva49.xml")
 teins = "http://www.tei-c.org/ns/1.0"
 xp = "/ns:TEI/ns:text/ns:body/ns:div"
 doc = readxml(f)
-
 
 
 function format_row(row)
@@ -22,28 +22,46 @@ function format_row(row)
     join(rowtext,"") * " |"
 end
 
-divs = findall(xp, root(doc),["ns"=> teins])
-for d in divs
-    psg = d["n"]            
-    tlist = findall("ns:table", d, ["ns"=> teins])
-    for t in tlist
-        if haskey(t, "n")
-            tab = t["n"]  
-            refbase = "$(psg).$(tab)"
-            @info(refbase)
-     
-            for r in  findall("ns:row", t,  ["ns"=> teins])
-                if haskey(r, "n")
-                    row = r["n"]
-                    ref = "$(refbase).$(row)"
-                    println(ref, "|", format_row(r))
+function j_divs(divlist, urnbase)
+    cex = []
+    for d in divlist
+        psg = d["n"]            
+        tlist = findall("ns:table", d, ["ns"=> teins])
+        for t in tlist
+            if haskey(t, "n")
+                tab = t["n"]  
+                refbase = "$(psg).$(tab)"
+                urn = "$(urnbase)$(refbase)"
+        
+                for r in  findall("ns:row", t,  ["ns"=> teins])
+                    @info("ROW")
+                    if haskey(r, "n")
+                        row = r["n"]
+                        ref = "$(urn).$(row)"
+                        push!(cex, string(ref, "|", format_row(r)))
+                        #@info("Pushed ", string(ref, "|", format_row(r)))
 
-                else
-                    @warn("NO n attr for row in $(refbase)")
+                    else
+                        @warn("NO n attr for row in $(refbase)")
+                    end
                 end
+            else
+                @warn("No n attr for table in div $(psg)")
             end
-        else
-            @warn("No n attr for table in div $(psg)")
         end
     end
+    join(cex,"\n")
+end
+
+gen49 = "urn:cts:latinLit:stoa0162.stoa005.gen49:"
+divs = findall(xp, root(doc),["ns"=> teins])
+cexall  = j_divs(divs, gen49)
+
+
+psgs = []
+for ln in split(cexall, "\n")
+    cols = split(ln, "|")
+    urn = CtsUrn(cols[1])
+    txt = "|" * join(cols[2:end], " | ") * " |"
+    push!(psgs, CitablePassage(urn,txt))
 end
